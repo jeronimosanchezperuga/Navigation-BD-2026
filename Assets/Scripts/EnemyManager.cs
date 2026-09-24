5c27bc0137a95fefd1e4a6b80167711daf6edc50
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2026-09-24 11:48:35 -0300

    Ejercicio enemies

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Enemy[] enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = FindObjectsOfType<Enemy>();
+        //SetDamagePointsTo(0);
+        SetRandomValueToDamagePoints();
+        DeactivateAllEnemies();
+    }
+
+    void SetDamagePointsTo(int newValue)
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].damagePoints = newValue;
+        }
+    }
+
+    void SetRandomValueToDamagePoints()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].damagePoints = Random.Range(1,11);
+        }
+    }
+
+    void DeactivateAllEnemies()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].gameObject.SetActive(false);
+        }
+    }
+}
